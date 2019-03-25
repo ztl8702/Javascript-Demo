ecedef8b92d981fff75c982a143b6d39ae79d886
Radium Zheng <ztl8702@msn.com>
2019-03-24 21:21:11 -0700

    Nerd -> Some

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,4 +1,4 @@
-angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+angular.module('NerdCtrl', []).controller('SomeController', function($scope) {
 
 	$scope.tagline = 'Nothing beats a pocket protector!';
 	$scope.name = "";
